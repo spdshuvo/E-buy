b607c01c72a35578de5e49396bdb5a3caa353bc3
spdshuvo <shuvodev47@gmail.com>
2021-07-15 17:49:20 +0600

    footer runing

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,18 @@
-$(function(){
+$(function () {
     'use strict';
     $(".banner_slider_container").slick();
-    
+
     $('.featured_filter_container').filterizr();
+    $(".latest_products_container").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrow:false,
+    })
+    $(".testimonial_item_container").slick({
+        slidesToShow: 2,
+        autoplay: true,
+        autoplaySpeed: 2000
+    })
 })
